566514cc5ad994f08677d8edfe46eb0f1a2942b5
Bartolomeo99 <bartosz2064@gmail.com>
2023-02-25 10:35:15 +0100

    first commit

--- Dockerfile
+++ Dockerfile
@@ -12,5 +12,6 @@ COPY ./requirements.txt /app/requirements.txt
 RUN pip install -r requirements.txt
 
 COPY . .
+EXPOSE 8010
 
-CMD python manage.py runserver 0.0.0.0:8000
+CMD ["python","manage.py","runserver","0.0.0.0:8010"]
--- Ejedzenie/settings.py
+++ Ejedzenie/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = '-95t%=#4o3@l-(-%ok9*h%n3!0(sdchjn%+_$5#umaj-!3bg*7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django.postgres.com', '192.168.39.31:31000']
 
 
 # Application definition
@@ -75,13 +75,24 @@ WSGI_APPLICATION = 'Ejedzenie.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST','db'),
+        'NAME': os.environ.get('DB_NAME','app'),
+        'USER': os.environ.get('DB_USER','postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD','12345'),
+        'PORT': os.getenv('POSTGRES_PORT', 5432)
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
